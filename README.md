76d68bf3e90bf54103caab81cd618791b5b2155a
Jeremy Ber <jdber@amazon.com>
2022-08-04 09:30:20 -0500

    first commit

--- README.md
+++ README.md
@@ -1,17 +1,38 @@
-## My Project
+# Maven archetype project for KDA specific Flink quickstart
 
-TODO: Fill this README out!
+This github project features a self-guided Kinesis Data Analytics for Apache Flink Maven Archetype to get you up and running with a Kinesis Data Analytics for Apache Flink application on your local machine simply by running a single command.
 
-Be sure to:
+## Prerequisites for your local machine
+- [Install Java 8 or 11](https://www.java.com/en/download/help/download_options.html) 
+- [Install Maven](https://maven.apache.org/install.html)
 
-* Change the title in this README
-* Edit your repository description on GitHub
+## Instructions
 
-## Security
+1. Clone this repository to your local machine
+2. Navigate to the root of the cloned repo from a command prompt on your local machine.
+3. Type `mvn install` to install the archetype to your local maven repository
+3. Move to a separate repository where you want to save your new flink project, and execute the following command.
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+### Note: 
+You can substitute values for your desired app name, flink version, etc in the command before executing it to generate a project to your specific needs.
 
-## License
+```bash
+mvn archetype:generate \
+    -DarchetypeGroupId=com.amazonaws.services.kinesisanalytics \
+    -DarchetypeArtifactId=flink-quickstart-kda-java \
+    -DarchetypeVersion=1.0-SNAPSHOT \
+    -DgroupId=com.amazonaws.services.kinesisanalytics \
+    -DartifactId=my-first-kda-application \
+    -Dversion=1.0.1 \
+    -Dpackage=com.amazonaws.services.kinesisanalytics \
+    -DinteractiveMode=false \
+    -Dflink-version=1.13.6 \
+    -Djava-version=1.11 \
+    -Djdk-version=11 \
+    -Dscala-binary-version=2.11 \
+    -Dkda-connectors-version=2.0.0 \
+    -Dkda-runtime-version=1.2.0 \
+    -Dlog4j-version=2.12.1
+```
 
-This library is licensed under the MIT-0 License. See the LICENSE file.
-
+Once you run this command, a project will be generated which contains all the necessary structure and code to get started developing a Kinesis Data Analytics for Apache Flink application.
---
+++ target/classes/archetype-resources/src/main/java/StreamingJob.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT-0
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package ${package};
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.datastream.DataStream;
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ *
+ * <p>For a tutorial how to write a Flink streaming application, check the
+ * tutorials and examples on the <a href="https://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class StreamingJob {
+
+	public static void main(String[] args) throws Exception {
+		// set up the streaming execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		/*
+		 * Here, you can start creating your execution plan for Flink.
+		 *
+		 * Start with getting some data from the environment, like
+		 * 	env.readTextFile(textPath);
+		 *
+		 * then, transform the resulting DataStream<String> using operations
+		 * like
+		 * 	.filter()
+		 * 	.flatMap()
+		 * 	.join()
+		 * 	.coGroup()
+		 *
+		 * and many more.
+		 * Have a look at the programming guide for the Java API:
+		 *
+		 * https://flink.apache.org/docs/latest/apis/streaming/index.html
+		 *
+		 */
+		DataStream<Long> someIntegers = env.fromSequence(0, 1000);
+
+		someIntegers.print();
+
+		// execute program
+		env.execute("Flink Streaming Java API Skeleton");
+	}
+}
